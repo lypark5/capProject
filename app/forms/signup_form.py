799e9aa599ce444c31aa47973aafbb21c4bccffd
Ludia <lypark1985@gmail.com>
2023-09-07 12:43:48 -0500

    fixed forms to aws

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms import StringField, SubmitField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms.validators import DataRequired, Email, ValidationError, Length, URL
 from app.models import User
+from app.api.aws_routes import ALLOWED_EXTENSIONS
 
 
 def user_exists(form, field):
@@ -21,7 +23,10 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
+    username = StringField('username', validators=[DataRequired(), username_exists])
+    first_name = StringField('First name', validators=[DataRequired(), Length(min=3, max=50)])
+    last_name = StringField('Last name', validators=[DataRequired(), Length(min=3, max=50)])
     email = StringField('email', validators=[DataRequired(), user_exists])
+    profile_pic = FileField('Profile Pic', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
     password = StringField('password', validators=[DataRequired()])
+    submit = SubmitField('Sign up')
