a729e3e01ab79ed1ef235e0ecaf54a76d1002909
Ludia <lypark1985@gmail.com>
2023-09-10 19:11:49 -0500

    fixed seed

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -24,13 +24,13 @@ def seed_comments():
   )
   comment5 = Comment(
     user_id=5,
-    recipe_ID=9,
+    recipe_id=9,
     comment="smdh"
   )
   comment6 = Comment(
     user_id=7,
     recipe_id=3,
-    comment="ty for this recipe, my mom loved it!"
+    comment="ty for this recipe, my mom loved it!",
     comment_pic='https://recipe-capstone-project.s3.us-east-2.amazonaws.com/herringReal.jpg'
   )
   comment7 = Comment(
@@ -38,6 +38,22 @@ def seed_comments():
   recipe_id=6,
   comment="IT'S RAWWWWWWWW!!!"
   )
-  comment8 = Comment(
-    user_id
-  )
+  # comment8 = Comment(
+  #   user_id
+  # )
+  db.session.add(comment1)
+  db.session.add(comment2)
+  db.session.add(comment3)
+  db.session.add(comment4)
+  db.session.add(comment5)
+  db.session.add(comment6)
+  db.session.add(comment7)
+  db.session.commit()
+
+def undo_comments():
+  if environment == "production":
+    db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
+  else:
+      db.session.execute(text("DELETE FROM comments"))
+
+  db.session.commit()
