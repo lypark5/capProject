069996570730abb9844bed52dca1a2851fc82ab7
Ludia <lypark1985@gmail.com>
2023-09-13 17:44:52 -0500

    create comment successful

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .create_recipe_form import CreateRecipeForm
 from .update_recipe_form import UpdateRecipeForm
+from .create_comment_form import CreateCommentForm
