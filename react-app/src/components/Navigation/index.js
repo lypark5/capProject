b4f3e2a7e474f866e5006b1e2dcc292859430fc0
Ludia <lypark1985@gmail.com>
2023-09-16 03:13:28 -0500

    some detail css

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,34 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import { useHistory } from 'react-router-dom/';
-import OpenModalButton from '../OpenModalButton';
-import RecipeFormFunction from '../Recipes/CreateRecipe';
-import { getAllRecipesThunk } from '../../store/recipes';					// why do i need this?
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
-	const dispatch = useDispatch()
-	const history = useHistory()
 	let homeUrl;																													// declare the url for home button
 	sessionUser ? homeUrl = "/recipes/all" : homeUrl = "/";								// if there is a logged-in user, make the home button go to get all photos, else make it go to landing page.
 
-	const createRecipeLinkFunction = (userId) => {												// this is for create a recipe link.
-		// dispatch(getAllRecipesThunk(userId))															// why do i need this
-		history.push("/albums/new")
-	}
-
 	return (
-		<div>
-		<div id='nav-div-to-center-logo'>
-			<NavLink to={homeUrl}>Logo</NavLink>
-			</div>
+		<div id='nav-bar'>
 			{isLoaded && (
-			
-					<ProfileButton user={sessionUser} />
-			
+				<ProfileButton user={sessionUser} />
+			)}
+			<NavLink to={homeUrl} id='logo'>Logo</NavLink>
+			{sessionUser && (
+				<NavLink to="/recipes/new" id='create-recipe-link-nav'>Upload a Recipe!</NavLink>
 			)}
 		</div>
 	);
