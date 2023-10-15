beefee91850790a1a6338ee246b770929613c9cb
Ludia <lypark1985@gmail.com>
2023-10-15 17:08:48 -0500

    some about me

--- react-app/src/components/AboutMe/index.js
+++ react-app/src/components/AboutMe/index.js
@@ -3,6 +3,7 @@ import './AboutMe.css';
 const AboutMePage = () => {
   return (
     <div id='about-overlord'>
+      <div></div>
       <img src='https://recipe-capstone-project.s3.us-east-2.amazonaws.com/specialCat.jpg' alt='special cat' />
       <h1>Meet the dev</h1>
 
