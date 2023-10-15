f3d9567a4b48959a3d735184ed484f8c072b7b3a
Ludia <lypark1985@gmail.com>
2023-10-15 18:13:11 -0500

    finished about me

--- react-app/src/components/AboutMe/index.js
+++ react-app/src/components/AboutMe/index.js
@@ -3,10 +3,21 @@ import './AboutMe.css';
 const AboutMePage = () => {
   return (
     <div id='about-overlord'>
-      <div></div>
-      <img src='https://recipe-capstone-project.s3.us-east-2.amazonaws.com/specialCat.jpg' alt='special cat' />
-      <h1>Meet the dev</h1>
-
+      <div id='about-meat'>
+        <img src='https://recipe-capstone-project.s3.us-east-2.amazonaws.com/specialCat.jpg' alt='special_cat' id='special-cat-pic'/>
+        <div id='about-words-div'> 
+          <span id='meet-title'>Meet the dev</span>
+          <p id='ludia-sentence'>Hi, I'm <strong><span style={{color:'rgb(0, 103, 193)'}}>Ludia Park</span></strong>.<br />Enjoy this cat.</p>
+          <div id='ludi-links-container'>
+            <a href='https://github.com/lypark5' id='ludi-link1' target='_blank'>
+              <i className='fab fa-github'></i>
+            </a>
+            <a href='https://www.linkedin.com/in/ludia-park-172496293/' id='ludi-link2' target='_blank'>
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
